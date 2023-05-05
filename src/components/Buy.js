33bf9101df9ad95e538741a9ebd3fac49d1c4ddd
lusenakalee <lusenakalee@gmail.com>
2023-05-05 15:19:03 +0300

    required

--- src/components/Buy.js
+++ src/components/Buy.js
@@ -134,6 +134,7 @@ function Buy() {
                       <input
                         type="text"
                         name="Device-information"
+                        required
                         id="Device-information"
                         placeholder='Brand and Model'
                         className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
